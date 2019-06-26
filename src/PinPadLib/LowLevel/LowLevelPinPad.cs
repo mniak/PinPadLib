008d5ea0f3722e560ae3cbd08d5e8c06f0f9921f
Andre Soares <andregsilv@gmail.com>
2019-06-26 00:19:32 -0300

    nothing

---
+++ src/PinPadLib/LowLevel/LowLevelPinPad.cs
@@ -0,0 +1,25 @@
+﻿using PinPadLib.Raw;
+using System;
+using System.Threading.Tasks;
+
+namespace PinPadLib.LowLevel
+{
+    public class LowLevelPinPad : ILowLevelPinPad
+    {
+        private readonly IRawPinPad rawPinPad;
+
+        public LowLevelPinPad(IRawPinPad rawPinPad)
+        {
+            this.rawPinPad = rawPinPad ?? throw new ArgumentNullException(nameof(rawPinPad));
+        }
+        public Task<LowLevelResponseMessage> ReceiveResponseAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendCommandAsync(LowLevelRequestMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
