8a1b6192d3ec39f37b8c69a151b22ae100969724
Andre Soares <andregsilv@gmail.com>
2019-06-26 00:40:30 -0300

    vou dormir

--- src/PinPadLib/LowLevel/LowLevelPinPad.cs
+++ src/PinPadLib/LowLevel/LowLevelPinPad.cs
@@ -1,5 +1,7 @@
 ﻿using PinPadLib.Raw;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PinPadLib.LowLevel
@@ -19,7 +21,14 @@ namespace PinPadLib.LowLevel
 
         public Task SendCommandAsync(LowLevelRequestMessage message)
         {
-            throw new NotImplementedException();
+            var bytes = new List<byte>();
+            bytes.AddRange(Encoding.ASCII.GetBytes(message.Command.acronym));
+            foreach (var parameter in message.Parameters)
+            {
+                bytes.AddRange(Encoding.ASCII.GetBytes(parameter.Length.ToString("000")));
+                bytes.AddRange(Encoding.ASCII.GetBytes(parameter));
+            }
+            return this.rawPinPad.SendRawMessageAsync(bytes.ToArray());
         }
     }
 }
--- tests/PinPadLib.LowLevel.UnitTests/LowLevelPinpadTest.cs
+++ tests/PinPadLib.LowLevel.UnitTests/LowLevelPinpadTest.cs
@@ -1,14 +1,30 @@
-using System;
+using AutoFixture.Xunit2;
+using Moq;
+using PinPadLib.LowLevel.UnitTests._Infra;
+using PinPadLib.Raw;
 using Xunit;
 
 namespace PinPadLib.LowLevel.UnitTests
 {
-    public class UnitTest1
+    public class LowLevelPinpadTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [AutoMoqData]
+        public void WhenSendMessageWithoutParameters_ShouldAdd000(
+            [Frozen]Mock<IRawPinPad> rawPinPadMock,
+            LowLevelPinPad sut)
         {
+            rawPinPadMock.Setup(x => x.SendRawMessageAsync(It.IsAny<RawRequestMessage>()));
 
+            sut.SendCommandAsync(new LowLevelRequestMessage(CommandName.Open));
+
+            var rawMsg = new ByteArrayBuilder()
+                .Add(Bytes.SYN)
+                .Add("OPN000")
+                .Add(Bytes.ETB)
+                .Add(0x77, 0x5e)
+                .ToArray();
+            rawPinPadMock.Verify(x => x.SendRawMessageAsync(It.Is<RawRequestMessage>(msg => msg.Equals(rawMsg))), Times.Once);
         }
     }
 }
